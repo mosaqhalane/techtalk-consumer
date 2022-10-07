19f1a788c76d0e56f3e1e2416a4ca7290ecb67a4
Mosa <mosa.qhalane@gmail.com>
2022-10-07 06:21:13 +0200

    corrected participant name

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -5,8 +5,8 @@ import { Product } from './product';
 const { eachLike, like, regex } = Matchers;
 
 const mockProvider = new Pact({
-  consumer: 'techtalk--consumer',
-  provider: process.env.PACT_PROVIDER ? process.env.PACT_PROVIDER : 'techtalk--provider',
+  consumer: 'techtalk-consumer',
+  provider: process.env.PACT_PROVIDER ? process.env.PACT_PROVIDER : 'techtalk-provider',
 });
 
 describe('API Pact test', () => {
